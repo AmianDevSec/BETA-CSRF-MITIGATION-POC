43af6d40b44c3849dae5172c563bfd9501915a0d
Amian A. <amiandevsec@gmail.com>
2025-10-15 22:10:55 +0000

    Create tiny-express-server.js

---
+++ tiny-express-server.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+// === MY "strict" CORS setup (adapted for localhost testing) ===
+
+// I only allow the same-origin origin (http://localhost:3000)
+const allowedOrigins = ["http://localhost:3000"];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    // Reject requests without Origin (treat as suspicious)
+    if (!origin) return callback(new Error("Origin header required"), false);
+    
+    if (allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      // return an error to cors middleware
+      callback(new Error("Not allowed by CORS"), false);
+    }
+  },
+  methods: ["GET","POST","PUT","DELETE"],
+  credentials: true,
+}));
+
+// Transfer endpoint
+app.post('/transfer', (req, res) => {
+  console.log('--- /transfer called ---');
+  console.log('Body:', req.body);
+  console.log('Cookies seen by server:', req.cookies);
+  res.send(`Transfer processed: ${req.body.amount} to ${req.body.to}`);
+});
+
+// Help page
+app.get('/', (req, res) => {
+  res.send("<h3>BETA CSRF MITIGATION POC, hosted on http://localhost:3000</h3>");
+});
+
+app.listen(3000, () => console.log('POC server running on http://localhost:3000'));
